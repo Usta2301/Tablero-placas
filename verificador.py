ec3664d670071dca93f4104d387b2aab2a8691aa
Usta2301 <aev.usta23@gmail.com>
2025-05-27 15:34:55 -0500

    Update verificador.py

--- verificador.py
+++ verificador.py
@@ -1,8 +1,1 @@
-import pandas as pd
 
-def cargar_placas_permitidas(ruta_excel="PlacasPermitidas.xlsx"):
-    df = pd.read_excel(ruta_excel)
-    return df['Placa'].tolist()
-
-def verificar_acceso(placa, lista_autorizada):
-    return "ACCESO PERMITIDO" if placa in lista_autorizada else "ACCESO DENEGADO"
